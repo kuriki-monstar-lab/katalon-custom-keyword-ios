a4e3ad36f18c40ae8567d98a297538a3f946ad74
chanyadeshani <chanya.deshani@gmail.com>
2018-07-17 14:59:13 +0900

    Adding a keyword to select value from UI picker

---
+++ Keywords/customkeywords/Picker_view.groovy
@@ -0,0 +1,69 @@
+package customkeywords
+
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testdata.TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords
+
+import internal.GlobalVariable
+
+import MobileBuiltInKeywords as Mobile
+import WSBuiltInKeywords as WS
+import WebUiBuiltInKeywords as WebUI
+
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.interactions.touch.TouchActions
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.By
+import org.openqa.selenium.Dimension
+import org.openqa.selenium.JavascriptExecutor
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory
+import com.kms.katalon.core.webui.driver.DriverFactory
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObjectProperty
+
+import com.kms.katalon.core.mobile.helper.MobileElementCommonHelper
+import com.kms.katalon.core.util.KeywordUtil
+
+import com.kms.katalon.core.webui.exception.WebElementNotFoundException
+import io.appium.java_client.AppiumDriver
+import io.appium.java_client.MobileDriver
+import io.appium.java_client.MobileElement
+import io.appium.java_client.TouchAction
+import io.appium.java_client.ios.IOSDriver
+
+public class picker_viewTap {
+
+	AppiumDriver<MobileElement> driver = MobileDriverFactory.getDriver()
+
+	/*
+	 * Send T text to Pickerview
+	 * eg: CustomKeywords.'custom_keywords.pickerviewTap.tapPickerview_mobile'('Women')
+	 * If the pickerview visible on the screen have value 'Women' this command will set value 'Women'.
+	 */
+	@Keyword
+	def tapPickerview_mobile(String elementToSet){
+		MobileElement ele1 = driver.findElementByClassName("XCUIElementTypePickerWheel")
+		ele1.sendKeys(elementToSet);
+	}
+}
+
+
+
